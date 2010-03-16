40d7eeb72dff5b4288f4f5173c3e60d4c963899f
Saulius Grigaitis <saulius.grigaitis@mif.vu.lt>
2010-03-16 17:55:11 +0200

    Change to one-liner for Fibonacci

--- examples/fibonacci/fibonacci.rb
+++ examples/fibonacci/fibonacci.rb
@@ -1,9 +1,5 @@
 class Integer
   def fibonacci
-    if self == 0 or self == 1
-      self 
-    else
-      (self - 2).fibonacci + (self - 1).fibonacci
-    end
+    self < 2 ? self : (self - 2).fibonacci + (self - 1).fibonacci
   end
 end
