441798589796dd73218d6de816464ca02043ecfd
Saulius Grigaitis <saulius.grigaitis@mif.vu.lt>
2010-03-16 17:37:41 +0200

    Fixed typo

--- examples/fibonacci/fibonacci_spec.rb
+++ examples/fibonacci/fibonacci_spec.rb
@@ -11,7 +11,7 @@ describe Integer, 'fibonacci' do
     2.fibonacci.should == 1
   end
 
-  it "should be 0 for third fibonacci number" do
+  it "should be 1 for third fibonacci number" do
     3.fibonacci.should == 1
   end
 
